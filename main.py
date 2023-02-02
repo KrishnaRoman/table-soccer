6cafc36227fdf46fc0453608bb01772fcdce4db7
KrishnaRoman <krishnaroman3009@gmail.com>
2023-02-01 19:03:53 -0500

    Emulate players collision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import pymunk
+import pymunk.pygame_util
 
 # Initialize the gygame
 pygame.init()
@@ -14,19 +16,52 @@ pygame.display.set_icon(icon)
 # Background
 background = pygame.image.load("field.jpg")
 
+# pymunk space
+space = pymunk.Space()
+static_body = space.static_body
+draw_options = pymunk.pygame_util.DrawOptions(screen)
+
+# clock
+clock = pygame.time.Clock()
+FPS = 120
+
+# funcion for creating balls
+def create_ball(radious, pos):
+    body = pymunk.Body()
+    body.position = pos
+    shape = pymunk.Circle(body, radious)
+    shape.mass = 5
+    # use pivot join to add friction
+    pivot = pymunk.PivotJoint(static_body, body, (0,0), (0,0))
+    pivot.max_bias = 0 # disable joint correction
+    pivot.max_force = 1000 # emulate linear friction
+
+    space.add(body, shape, pivot)
+    return shape
+
+new_ball = create_ball(25, (300, 300))
+cue_ball = create_ball(25, (310, 700))
+
 # Game Loop
 running = True
 while running:
 
+    clock.tick(FPS)
+    space.step(1/FPS)
+
     # RGB background color
-    screen.fill((255,255,255))
+    screen.fill((0,0,0))
     screen.blit(background, (0,0))
 
     #Background Image
     screen.blit(background, (0,0))
 
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            cue_ball.body.apply_impulse_at_local_point((0, -1500), (0, 0))
         if event.type == pygame.QUIT:
             running = False
+
+    space.debug_draw(draw_options)
 
     pygame.display.update()
